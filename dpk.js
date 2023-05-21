bc2e0b890e2171024ee3aec9d1ec7c46bb04d6a8
AbdelRahman Hamed <email4.m.rahman@gmail.com>
2023-05-21 22:25:13 +0300

    refactor deterministicPartitionKey

--- dpk.js
+++ dpk.js
@@ -1,28 +1,22 @@
 const crypto = require("crypto");
 
+const createHash = (data) =>  crypto.createHash("sha3-512").update(data).digest("hex");
+
+const getCandidate = (event) => {
+  const {partitionKey} = event;
+  if (partitionKey) {
+    return typeof partitionKey !== "string" ? JSON.stringify(partitionKey) : partitionKey;
+  }
+  const data = JSON.stringify(event);
+  return createHash(data);
+}
+
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
-
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
-  }
-
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
+  const candidate = getCandidate(event) || TRIVIAL_PARTITION_KEY;
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+    return createHash(candidate);
   }
   return candidate;
 };
